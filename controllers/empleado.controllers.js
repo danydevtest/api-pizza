1f0990da6bc62ac3ce2cadd53e74353d9e7ec032
Dany Cambrano <danycambrano1@gmail.com>
2023-10-04 13:51:06 -0600

    update

--- controllers/empleado.controllers.js
+++ controllers/empleado.controllers.js
@@ -21,5 +21,69 @@ export default{
             });
             next(error);
         }
-    }
+    },
+    obtenerEmpleados: async(req,res,next)=>{
+        try {
+           const consulta = await models.Empleado.find();
+           res.json(consulta);
+        } catch (error) {
+            res.status(500).send({
+                message:"Ocurrió un error al consultar"
+            });
+            next(error);
+        }
+    },
+
+    obtenerEmpleado: async(req,res,next)=>{
+        try {
+                      
+            const consultaOne= await models.Empleado.findById(req.params.id);
+          if(!consultaOne){
+            
+            res.status(404).send({
+                message:"Dato no encontrado"
+                
+            });
+          }else{
+           res.status(200).json(consultaOne);
+          }
+            
+           
+        } catch (error) {
+            res.status(500).send({
+                message:"Ocurrió un error al consultar el empleado"
+            });
+            next(error);
+        }
+    },
+    deleteEmpleado: async(req,res,next)=>{
+        try {
+           await models.Empleado.findByIdAndDelete(req.params.id);
+           res.status(200).send({
+            message: "El empleado se ha eliminado correctamente"
+           });
+        } catch (error) {
+            res.status(500).send({
+                message:"Ocurrió un error al eliminar el empleado"
+            });
+            next(error);
+        }
+    },
+
+    updateEmpleado: async(req,res,next)=>{
+        try {
+            const updateDatos=req.body;
+
+            await models.Empleado.findByIdAndUpdate(req.params.id,updateDatos);
+            res.status(200).send({
+                message: "Datos actualizados correctamente"
+            })
+        } catch (error) {
+            res.status(500).send({
+                message:"Ocurrió un error al actualizar el empleado"
+            });
+            next(error);
+        }
+        }
+    
 }
--- routes/empleado.routes.js
+++ routes/empleado.routes.js
@@ -1,10 +1,14 @@
 import routerx from "express-promise-router";
 
 import EmpleadoController from "../controllers/empleado.controllers";
+import empleadoControllers from "../controllers/empleado.controllers";
 
 
 const router=routerx();
 
 router.post('/addEmpleado',EmpleadoController.addEmpleado);
-
+router.get("/getEmpleados",EmpleadoController.obtenerEmpleados);
+router.get("/getEmpleado/:id",EmpleadoController.obtenerEmpleado);
+router.delete("/deleteEmpleado/:id",EmpleadoController.deleteEmpleado);
+router.patch("/updateEmpleado/:id", empleadoControllers.updateEmpleado);
 export default router;
